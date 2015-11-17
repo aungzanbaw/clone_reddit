5303b964f5b803a4102325f6108b824d10ed67c2
aungzanbaw <aungzanbaw@gmail.com>
2015-11-17 14:29:52 +0630

    Authorization on links

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20151117054519) do
     t.integer  "user_id"
   end
 
+  add_index "links", ["user_id"], name: "index_links_on_user_id"
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
